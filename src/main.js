6baea71d9fc98b35b9252b6df815350ee67352ba
rheber <rheber@rediffmail.com>
2018-09-02 12:10:27 +1000

    React devtools extension

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 const {app, BrowserWindow} = require('electron');
+const {
+  default: installExtension,
+  REACT_DEVELOPER_TOOLS
+} = require('electron-devtools-installer');
+
 
 function createWindow() {
   console.log('Initialising window.');
+
+  installExtension(REACT_DEVELOPER_TOOLS).then(
+    name => console.log(`Added extension: ${name}`)
+  ).catch(
+    err => console.log(`Error: ${err}`)
+  );
+
   win = new BrowserWindow({height: 600, width: 800});
   win.loadURL('http://localhost:9001');
 }
