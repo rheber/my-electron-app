92496378a857ddce0368dafe42e3edbacf460f7d
rheber <rheber@rediffmail.com>
2018-09-02 16:06:21 +1000

    React-router

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
+import {Route} from 'react-router-dom';
 import './style.css';
+
+import Screen1 from 'components/Screen1';
+import Screen2 from 'components/Screen2';
 
 class App extends React.Component {
   render() {
     return (
-      <p>Ahoy!</p>
+      <React.Fragment>
+        <Route exact path='/' component={Screen1} />
+        <Route path='/screen2' component={Screen2} />
+      </React.Fragment>
     );
   }
 }
---
+++ app/components/Screen1/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default class extends React.Component {
+  render() {
+    return (
+      <Link to='/screen2'>Next</Link>
+    );
+  }
+}
