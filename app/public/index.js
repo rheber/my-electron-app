c2bc4c36007f0db729222efb229dfa1d680fc57b
rheber <rheber@rediffmail.com>
2018-09-02 14:42:05 +1000

    cross-env

--- app/public/index.js
+++ app/public/index.js
@@ -1,38 +1,29 @@
-const fs = require('fs');
-const path = require('path');
-
-const envDevPath = path.resolve(__dirname, '../../.env.dev');
-if(fs.existsSync(envDevPath)) {
-  require('dotenv').config({path: envDevPath});
-  console.log('Loaded development env file.');
-} else {
-  console.log('No env file loaded.');
-}
-
 const {app, BrowserWindow} = require('electron');
-const {
-  default: installExtension,
-  REACT_DEVELOPER_TOOLS
-} = require('electron-devtools-installer');
-
 
 function createWindow() {
   console.log(`Initialising window. Environment: ${process.env.NODE_ENV}`);
 
-  require('devtron').install();
-
-  installExtension(REACT_DEVELOPER_TOOLS).then(
-    name => console.log(`Added extension: ${name}`)
-  ).catch(
-    err => console.log(`Error: ${err}`)
-  );
-
   win = new BrowserWindow({height: 600, width: 800});
   if(process.env.NODE_ENV === 'development') {
+    const {
+      default: installExtension,
+      REACT_DEVELOPER_TOOLS
+    } = require('electron-devtools-installer');
+
+    require('devtron').install();
+
+    installExtension(REACT_DEVELOPER_TOOLS).then(
+      name => console.log(`Added extension: ${name}`)
+    ).catch(
+      err => console.log(`Error: ${err}`)
+    );
+
     win.loadURL('http://localhost:9001');
   } else {
     win.loadFile('./index.html');
   }
 }
+
+app.on('window-all-closed', app.quit);
 
 app.on('ready', createWindow);
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const config = {
   devServer: {
     contentBase: path.join(__dirname, 'app/public/'),
     port: 9001,
-    publicPath: 'http://localhost:9001/dist/',
+    publicPath: 'http://localhost:9001/dist/app/',
     hotOnly: true
   },
   entry: './app/index.js',
@@ -23,8 +23,8 @@ const config = {
     ]
   },
   output: {
-    path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, 'dist/app/'),
+    publicPath: '/dist/app/',
     filename: 'bundle.js'
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
@@ -33,7 +33,8 @@ const config = {
       path.resolve('./app/'),
       path.resolve('./node_modules/')
     ]
-  }
+  },
+  target: 'electron-main'
 };
 
 module.exports = config;
